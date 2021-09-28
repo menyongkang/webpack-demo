3dfc7f120421a942d3bf1ae0899bf2c7893aed3e
menyongkang <1075162046@qq.com>
2021-09-28 13:59:31 +0800

    1.add config.js ; fix nmp script ; install webpack & webpack-cli ; modularization

--- REDAME.md
+++ REDAME.md
@@ -1,1 +1,7 @@
-webpack 学习Demo 
+# webpack 学习 Demo
+
+## webpack 参数
+
+## Loader
+
+> 模块 loader 可以链式调用，链中的每个 loader 都对资源进行转化。链会逆序执行，第一个 loader 将其转化的资源传递给下一个 loader 。
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,9 +1,26 @@
+/**
+ *
+ * 集中管理webpack 配置
+ *
+ */
+
 const path = require("path");
 
 module.exports = {
-	entry: "../src/index.js",
+	// 入口
+	entry: "./src/index.js",
+	// 出口
 	output: {
-		filename: "main.js",
-		path: path.resolve(__dirname, "dist"),
+		filename: "main.js", //出口文件名称
+		path: path.resolve(__dirname, "../dist"), // 出口文件地址
+	},
+	//Loader
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
 	},
 };
