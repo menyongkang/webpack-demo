9a148262d07f3568721e6ff5082f1228b34d062f
menyongkang <1075162046@qq.com>
2021-09-29 17:16:58 +0800

    config images and fonts loader

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -14,12 +14,24 @@ module.exports = {
 		filename: "main.js", //出口文件名称
 		path: path.resolve(__dirname, "../dist"), // 出口文件地址
 	},
+	// mode："development",
 	//Loader
 	module: {
 		rules: [
 			{
+				// 处理css
 				test: /\.css$/i,
 				use: ["style-loader", "css-loader"],
+			},
+			{
+				// 处理 images
+				test: /\.(png|svg|jpg|jpeg|git)$/i,
+				type: "asset/resource",
+			},
+			{
+				// 处理字体
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
 			},
 		],
 	},
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from "lodash";
-import "./index.css";
+import "./styles/index.css";
+import WebpackImages from "./assets/images/webpack.png";
 
 function component() {
 	const element = document.createElement("div");
 	element.innerHTML = _.join(["Hello", "webpack"], "");
 	element.classList.add("hello");
+
+	const images = new Image();
+	images.src = WebpackImages;
+	element.appendChild(images);
+
 	return element;
 }
 
