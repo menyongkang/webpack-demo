6bbfdbbec222e248410424bd3e6b0947103e1f87
menyongkang <1075162046@qq.com>
2021-10-08 14:34:42 +0800

    1:add JOSON parser; csv & xml loader

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -5,7 +5,10 @@
  */
 
 const path = require("path");
-
+//导入自定义 JSON 模块 parser
+const toml = require("../src/data/data.toml");
+const yaml = require("../src/data/data.yaml");
+const json5 = require("../src/data/data.json5");
 module.exports = {
 	// 入口
 	entry: "./src/index.js",
@@ -50,6 +53,39 @@ module.exports = {
 				// 处理字体
 				test: /\.(woff|woff2|eot|ttf|otf)$/i,
 				type: "asset/resource",
+			},
+			{
+				// 转化 csv tsv文件
+				test: /\.(csv|tsv)$/i,
+				use: ["csv-loader"],
+			},
+			{
+				// 转化xml
+				test: /\.xml$/i,
+				use: ["xml-loader"],
+			},
+			{
+				test: /\.toml$/i,
+				type: "json",
+				// 解析器, 解析选项对象。
+				parser: {
+					//它可能在没有特定加载器的时候,自定义的逻辑，以解析模块的源和并将它转换成 JavaScript 对象
+					parse: toml.parse,
+				},
+			},
+			{
+				test: /\.yaml$/i,
+				type: "json",
+				parser: {
+					parse: yaml.parse,
+				},
+			},
+			{
+				test: /\.json5$/i,
+				type: "json",
+				parser: {
+					parse: json5.parse,
+				},
 			},
 		],
 	},
--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import _ from "lodash";
 import "./styles/index.css";
 import "../src/assets/iconfont/iconfont.css";
 import WebpackImages from "./assets/images/webpack.png";
+import DataXml from "../src/data/data.xml";
+import Notes from "../src/data/data.csv";
+import toml from "../scr/data/data.toml";
+import yaml from "../scr/data/data.yaml";
+import json from "../scr/data/data.json5";
+
+console.log("toml: ", toml.title);
+console.log("toml: ", toml.owner.name);
+
+console.log("yaml: ", yaml.title);
+console.log("yaml: ", yaml.owner.name);
+
+console.log("json: ", json.title);
+console.log("json: ", json.owner.name);
 
 function component() {
 	const element = document.createElement("div");
@@ -11,6 +25,9 @@ function component() {
 	const images = new Image();
 	images.src = WebpackImages;
 	element.appendChild(images);
+
+	console.log(DataXml);
+	console.log(Notes);
 
 	return element;
 }
